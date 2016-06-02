cff3f7855b5f674b193699eec3de3de3bd9e4e36
Vlad Tokarev <tokarev1c@yandex.ru>
2016-06-02 16:24:34 +0300

    Изменил код генерации базы данных

--- db/mysql_create.sql
+++ db/mysql_create.sql
@@ -1,26 +1,7 @@
-CREATE TABLE `DbNameTemplate` (
+CREATE TABLE `Bookmakers` (
 	`id` bigint NOT NULL,
 	`Name` char(50),
 	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Countries` (
-	`id` bigint NOT NULL,
-	`name` char(100) NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `Leagues` (
-	`id` bigint NOT NULL,
-	`Country` bigint NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `LeagueNames` (
-	`Name` char(250) NOT NULL AUTO_INCREMENT,
-	`Bookmaker` bigint NOT NULL AUTO_INCREMENT,
-	`League` bigint NOT NULL,
-	PRIMARY KEY (`Name`,`Bookmaker`)
 );
 
 CREATE TABLE `Participants` (
@@ -37,7 +18,6 @@ CREATE TABLE `ParticipantNames` (
 
 CREATE TABLE `FootballEvents` (
 	`id` bigint NOT NULL,
-	`League` bigint NOT NULL,
 	`FirstParticipant` bigint NOT NULL,
 	`SecondParticipant` bigint NOT NULL,
 	`EventDate` DATETIME NOT NULL,
@@ -51,26 +31,38 @@ CREATE TABLE `FootballBets` (
 	`FirstWin` double NOT NULL,
 	`SecondWin` double NOT NULL,
 	`Draw` double NOT NULL,
+	`Handicap` bigint NOT NULL,
+	`Total` bigint NOT NULL,
 	PRIMARY KEY (`FootballEvent`,`Bookmaker`)
 );
 
-ALTER TABLE `Leagues` ADD CONSTRAINT `Leagues_fk0` FOREIGN KEY (`Country`) REFERENCES `Countries`(`id`);
+CREATE TABLE `Handicap` (
+	`id` bigint NOT NULL,
+	`ForwardValue` double NOT NULL,
+	`Coff` double NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-ALTER TABLE `LeagueNames` ADD CONSTRAINT `LeagueNames_fk0` FOREIGN KEY (`Bookmaker`) REFERENCES `Bookmakers`(`id`);
-
-ALTER TABLE `LeagueNames` ADD CONSTRAINT `LeagueNames_fk1` FOREIGN KEY (`League`) REFERENCES `Leagues`(`id`);
+CREATE TABLE `Total` (
+	`id` bigint NOT NULL,
+	`Total_under` bool NOT NULL,
+	`TotalValue` double NOT NULL,
+	`Coff` double NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
 ALTER TABLE `ParticipantNames` ADD CONSTRAINT `ParticipantNames_fk0` FOREIGN KEY (`Bookmaker`) REFERENCES `Bookmakers`(`id`);
 
 ALTER TABLE `ParticipantNames` ADD CONSTRAINT `ParticipantNames_fk1` FOREIGN KEY (`Participant`) REFERENCES `Participants`(`id`);
 
-ALTER TABLE `FootballEvents` ADD CONSTRAINT `FootballEvents_fk0` FOREIGN KEY (`League`) REFERENCES `Leagues`(`id`);
+ALTER TABLE `FootballEvents` ADD CONSTRAINT `FootballEvents_fk0` FOREIGN KEY (`FirstParticipant`) REFERENCES `Participants`(`id`);
 
-ALTER TABLE `FootballEvents` ADD CONSTRAINT `FootballEvents_fk1` FOREIGN KEY (`FirstParticipant`) REFERENCES `Participants`(`id`);
-
-ALTER TABLE `FootballEvents` ADD CONSTRAINT `FootballEvents_fk2` FOREIGN KEY (`SecondParticipant`) REFERENCES `Participants`(`id`);
+ALTER TABLE `FootballEvents` ADD CONSTRAINT `FootballEvents_fk1` FOREIGN KEY (`SecondParticipant`) REFERENCES `Participants`(`id`);
 
 ALTER TABLE `FootballBets` ADD CONSTRAINT `FootballBets_fk0` FOREIGN KEY (`FootballEvent`) REFERENCES `FootballEvents`(`id`);
 
 ALTER TABLE `FootballBets` ADD CONSTRAINT `FootballBets_fk1` FOREIGN KEY (`Bookmaker`) REFERENCES `Bookmakers`(`id`);
 
+ALTER TABLE `FootballBets` ADD CONSTRAINT `FootballBets_fk2` FOREIGN KEY (`Handicap`) REFERENCES `Handicap`(`id`);
+
+ALTER TABLE `FootballBets` ADD CONSTRAINT `FootballBets_fk3` FOREIGN KEY (`Total`) REFERENCES `Total`(`id`);
