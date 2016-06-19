85bfb49ab14019f9bda65e773ff877221d806c03
Vlad Tokarev <tokarev1c@yandex.ru>
2016-06-19 11:25:32 +0300

    Автоматическое добавление контор

--- tests/test2.py
+++ tests/test2.py
@@ -1,4 +1,6 @@
 import scraping.sbobet as scraping_module
+import scraping.mar1 as scraping_module2
 from download import download_handicaps
 
 download_handicaps(scraping_module)
+download_handicaps(scraping_module2)
