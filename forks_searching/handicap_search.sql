9dbaeeeef1ace0ea05ae89ccda1f843aae8609d2
Vlad Tokarev <like.tokarev@gmail.com>
2016-06-28 12:29:52 +0000

    12

--- forks_searching/handicap_search.sql
+++ forks_searching/handicap_search.sql
@@ -18,5 +18,26 @@ CREATE TEMPORARY TABLE IF NOT EXISTS forks (SELECT h1.firstparticipant,
         AND h1.actual
         AND h2.actual
         AND ( h1.firstforward + h2.secondforward ) >= 0)
+  UNION
+  (SELECT h1.firstparticipant,
+        h2.firstparticipant,
+        h1.firstforward,
+        h2.firstforward,
+        h1.firstwin,
+        h2.firstwin,
+        h1.bookmaker                 AS bk1,
+        h2.bookmaker                 AS bk2,
+        h1.href as href1,
+        h2.href as href2,
+        ( h1.firstwin * h2.firstwin - h1.firstwin - h2.firstwin ) / (
+        h1.firstwin + h2.firstwin ) AS merge_percent
+ FROM   handicaps AS h1,
+        handicaps AS h2
+ WHERE  h1.firstparticipant = h2.secondparticipant
+        AND h1.secondparticipant = h2.firstparticipant
+        AND h1.bookmaker != h2.bookmaker
+        AND h1.actual
+        AND h2.actual
+        AND ( h1.firstforward + h2.firstforward ) >= 0)
 
 
