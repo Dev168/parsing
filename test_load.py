d4a01cec1ca112b7770da789ab8ec7f90edb0101
Vlad <like.tokarev@gmail.com>
2016-07-30 10:56:00 +0300

    фронт енд версия 1.0

--- test_load.py
+++ test_load.py
@@ -9,22 +9,21 @@ m = Marathonbet()
 sbf = sb.get_scraping_urls()
 mf = m.get_scraping_urls()
 
-# while True:
-for f in sbf:
-    try:
-        sb.download_events(f)
-    except (IndexError, AttributeError, KeyError, GetSportPageException, sb._timeoutexception):
-        pass
+while True:
+    for f in sbf:
+        try:
+            sb.download_events(f)
+        except (IndexError, AttributeError, KeyError, GetSportPageException, sb._timeoutexception):
+            pass
 
-for f in mf:
-    try:
-        m.download_events(f)
-    except (IndexError, AttributeError, KeyError, GetSportPageException, sb._timeoutexception):
-        pass
+    for f in mf:
+        try:
+            m.download_events(f)
+        except (IndexError, AttributeError, KeyError, GetSportPageException, sb._timeoutexception):
+            pass
 
-
-print("=========================")
-print("Delay 2 sec")
-print("=========================")
-sleep(2)
+    print("=========================")
+    print("Delay 2 sec")
+    print("=========================")
+    sleep(1)
 
