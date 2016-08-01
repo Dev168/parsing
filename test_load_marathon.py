1c11bab91dd49f333c186307128536efe4f0f8f9
Vlad <like.tokarev@gmail.com>
2016-08-01 13:04:49 +0300

    Выделил в два модуля

--- test_load_marathon.py
+++ test_load_marathon.py
@@ -3,19 +3,16 @@ from bookmakers.Sbobet import Sbobet
 from bookmakers.Marathonbet import Marathonbet
 from time import sleep
 
-sb = Sbobet()
-
-
-sbf = sb.get_scraping_urls()
-
+m = Marathonbet()
+mf = m.get_scraping_urls()
 
 while True:
-    for f in sbf:
-        try:
-            sb.download_events(f)
-        except (IndexError, AttributeError, KeyError, GetSportPageException, sb._timeoutexception):
-            pass
 
+    for f in mf:
+        try:
+            m.download_events(f)
+        except (IndexError, AttributeError, KeyError, GetSportPageException, m._timeoutexception):
+            pass
 
     print("=========================")
     print("Delay 1 sec")
