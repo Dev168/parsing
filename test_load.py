24ced16999f4950afd5d38bd7d624ebdb2098b79
Vlad <like.tokarev@gmail.com>
2016-08-02 19:29:12 +0300

    Исправил кое что

--- test_load.py
+++ test_load.py
@@ -19,7 +19,7 @@ while True:
         except (IndexError, AttributeError, KeyError, sb._timeoutexception):
             timelapse = random.randint(30, 180)
             timez = (datetime.utcnow() + timedelta(hours=3)).strftime("%H:%M:%S: ")
-            print(timez + "Сервер забанил нас. Сделаем паузу {0} секунд".format(timelapse))
+            print(timez + "Server banned us. Waiting {0} seconds".format(timelapse))
             sleep(timelapse)
 
     timelapse = random.randint(1, 3)
