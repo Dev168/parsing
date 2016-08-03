277ac008ccb176b7554450a297d52acbb97de330
Vlad <like.tokarev@gmail.com>
2016-08-03 18:10:50 +0300

    Убрал из событий все кроме названий лиг и игр

--- static/js/events.js
+++ static/js/events.js
@@ -47,39 +47,6 @@ function fillTables(data) {
                 </table>`
                 );
 
-                var moneylines = participantsList[participants]["moneylines"];
-                var handicaps = participantsList[participants]["handicaps"];
-
-                var tableMoneyLines = $(`<table class="table table-condensed table-bordered text-center moneylines"></table>`);
-                for (var moneyline in moneylines) {
-                    moneylineObj = moneylines[moneyline];
-
-                    if(!$.trim($("#time").html())){
-                        $("#time").append(moneylineObj["oddsdate"])}
-
-                    tableMoneyLines.append(`
-                            <tr>
-                                <td>` + moneylineObj["firstwin"] + `</td>
-                                <td>` + moneylineObj["draw"] + `</td>
-                                <td>` + moneylineObj["secondwin"] + `</td>
-                             </tr>
-                    `);
-                }
-                dataDiv.append(tableMoneyLines);
-
-                var tableHandicaps = $(`<table class="table table-condensed table-bordered text-center handicaps"></table>`);
-                for (var handicap in handicaps) {
-                    handicapObj = handicaps[handicap];
-                    tableHandicaps.append(`
-                            <tr>
-                                <td>` + handicapObj["firstforward"] + `</td>
-                                <td>` + handicapObj["firstwin"] + `</td>
-                                <td>` + handicapObj["secondforward"] + `</td>
-                                <td>` + handicapObj["secondwin"] + `</td>
-                             </tr>
-                    `);
-                }
-                dataDiv.append(tableHandicaps);
 
             }
         }
